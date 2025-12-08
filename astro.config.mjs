2e3b57af77b654fb8a5952e57a1fd7ab44433057
monkeytower <ok@monkeytower.net>
2025-12-08 03:37:09 +0100

    fix(astro): set output to static

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import keystatic from '@keystatic/astro';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'hybrid',
+  output: 'static',
   vite: {
     plugins: [tailwindcss()]
   },
